0d2593e0aa176013022ece938bf2bfab8c4f2767
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-05-01 22:04:35 +0300

    best View

--- ColorGeneration/ViewController.swift
+++ ColorGeneration/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var blueValueText: UITextField!
+    @IBOutlet var greenValueText: UITextField!
+    @IBOutlet var redValueText: UITextField!
+    
+    @IBOutlet var sliderRedOutlet: UISlider!
+    @IBOutlet var sliderGreenOutlet: UISlider!
+    @IBOutlet var sliderBlueOutlet: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sliderRedAction() {
+        redValueText.text = sliderRedOutlet.value.formatted()
+    }
 
 }
 
