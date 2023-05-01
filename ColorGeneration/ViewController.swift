a57118de5c7e2015bccde6619c89a4882e9c8d06
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-05-01 21:15:34 +0300

    Initial Commit

---
+++ ColorGeneration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorGeneration
+//
+//  Created by Роман on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
