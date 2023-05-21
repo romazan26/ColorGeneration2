4184f44502783db2693db0920752292ca5389f27
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-05-21 19:15:37 +0300

    newWelcomView

---
+++ ColorGeneration/ViewControllers/WelcomViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomViewController.swift
+//  ColorGeneration
+//
+//  Created by Роман on 21.05.2023.
+//
+
+import UIKit
+
+class WelcomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
